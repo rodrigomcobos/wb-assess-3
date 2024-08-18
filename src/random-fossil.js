96430072e62a91181ecbd550d0b233e189c59f00
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-08-17 23:46:33 -0600

    making small changes

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -3,14 +3,13 @@ import axios from 'axios';
 document
   .getElementById('get-random-fossil')
   .addEventListener('click', async () => {
-    try {
-      const response = await axios.get('/random-fossil.json');
-      const fossil = response.data;
-      document.getElementById(
-        'random-fossil-image'
-      ).innerHTML = `<img src="${fossil.img}" class="centered" />`;
-      document.getElementById('random-fossil-name').textContent = fossil.name;
-    } catch (error) {
+    const response = await axios.get('/random-fossil.json').catch((error) => {
       console.error(error);
-    }
+      return { data: { img: '', name: '' } };
+    });
+    const fossil = response.data;
+    document.getElementById(
+      'random-fossil-image'
+    ).innerHTML = `<img src="${fossil.img}" class="centered" />`;
+    document.getElementById('random-fossil-name').textContent = fossil.name;
   });
